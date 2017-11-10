2e0ea28376c83bb5aeb89763b6358e8bb54566a1
Ska <skalyxe@yahoo.fr>
2017-11-10 05:44:25 +0100

    +correction mcd et sql -> suppression heritage

--- loveletters.sql
+++ loveletters.sql
@@ -2,25 +2,35 @@ CREATE TABLE Utilisateur
 (
 	idUtilisateur integer(10) primary key not null,
 	login varchar(25),
-	password varchar(25)
+	pwd varchar(25)
 );
 
 CREATE TABLE Joueur
 (
 	idJoueur integer(10) primary key not null,
 	idUtilisateur integer(10) not null,
-	idDefausse integer(10) not null,
 	score integer(10),
-	foreign key (idUtilisateur) references Utilisateur(idUtilisateur),
-	foreign key (idDefausse) references Defausse(idDefausse)
+	foreign key (idUtilisateur) references Utilisateur(idUtilisateur)
 );
 
 CREATE TABLE Defausse
 (
 	idDefausse integer(10) primary key not null,
-	idJoueur integer(10) not null,
-	foreign key (idJoueur) references Joueur(idJoueur)
-) INHERITS (Carte);
+	idJoueur integer(10),
+	idManche integer(10) not null,
+	foreign key (idJoueur) references Joueur(idJoueur),
+	foreign key (idManche) references Manche(idManche)
+);
+
+CREATE TABLE EstPlacee
+(
+	idDefausse integer(10) not null,
+	idCarte integer(10) not null,
+	nbCartes integer(10) not null,
+	primary key (idDefausse, idCarte),
+	foreign key (idDefausse) references Defausse(idDefausse),
+	foreign key (idCarte) references Carte(idCarte)
+);
 
 CREATE TABLE Carte
 (
@@ -36,6 +46,7 @@ CREATE TABLE Possede
 	idJoueur integer(10) not null,
 	idCarte integer(10) not null,
 	nbCartes integer(10),
+	primary key (idJoueur, idCarte),
 	foreign key (idCarte) references Carte(idCarte),
 	foreign key (idJoueur) references Joueur(idJoueur)
 );
@@ -50,6 +61,7 @@ CREATE TABLE Comporte
 	idPioche integer(10) not null,
 	idCarte integer(10) not null,
 	nbCartes integer(10),
+	primary key (idPioche, idCarte),
 	foreign key (idPioche) references Pioche(idPioche),
 	foreign key (idCarte) references Carte(idCarte)
 );
@@ -59,21 +71,20 @@ CREATE TABLE Manche
 	idManche integer(10) primary key not null,
 	idPartie integer(10) not null,
 	idPioche integer(10) not null,
-	idDefausse integer(10) not null,
 	foreign key(idPioche) references Pioche(idPioche),
-	foreign key(idDefausse) references Defausse(idDefausse),
 	foreign key(idPartie) references Partie(idPartie)
 );
 
 CREATE TABLE Partie
 (
-	idPartie integer(10) primary key not null,
+	idPartie integer(10) primary key not null
 );
 
 CREATE TABLE Participe
 (
 	idJoueur integer(10) not null,
 	idManche integer(10) not null,
+	primary key(idJoueur, idManche),
 	foreign key(idJoueur) references Joueur(idJoueur),
 	foreign key(idManche) references Manche(idManche)
 );
