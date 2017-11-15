210af3e0ae81c0519a3a9ff346d60e9994e13cfb
Ska <skalyxe@yahoo.fr>
2017-11-15 22:10:34 +0100

    Ajout des images

--- projet/src/loveletters/controler/ControlerTest.php
+++ projet/src/loveletters/controler/ControlerTest.php
@@ -3,6 +3,7 @@
 namespace loveletters\controler;
 
 use \loveletters\model\Utilisateur;
+use \loveletters\model\Carte;
 use \loveletters\view\VueCarte;
 use \loveletters\controler\ControlerPartie;
 use \loveletters\model\DBConnection;
@@ -10,12 +11,26 @@ use \loveletters\model\DBConnection;
 class ControlerTest {
     public function index(){
           DBConnection::getInstance();
-          $idUtilisateurs=array();
-          $utilisateurs=Utilisateur::get();
-          foreach($utilisateurs as $user){
-            $idUtilisateurs[]=$user->idUtilisateur;
-          }
-          $con = new ControlerPartie();
-          $con->pioche(20,12);
+          $cartes=Carte::get();
+          $app=\Slim\Slim::getInstance();
+          $routecss=$app->urlFor('css');
+          $vue = new VueCarte();
+          $res=$vue->cardsToHtml($cartes);
+          $html=<<<HTML
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="$routecss/style.css">
+<title>Test Page</title>
+</head>
+<body>
+$res
+</body>
+</html>
+HTML;
+    echo $html;
+          //$con = new ControlerPartie();
+          //$con->pioche(20,12);
     }
 }
